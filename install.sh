0a0c45111928674a6dca3e8a45e0f4c88438db92
Bensuperpc <bensuperpc@gmail.com>
2020-10-31 19:23:23 +0100

    Add files

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Install.sh - Install for Manjaro and ArchLinux
+#
+# Created by Bensuperpc at 31, October of 2020
+#
+# Released into the Public domain with MIT licence
+# https://opensource.org/licenses/MIT
+#
+# Written with VisualStudio code 1.49.1 and python 3.8.5
+# Script compatibility : Linux (Manjaro and Archlinux based)
+#
+# ==============================================================================
+
+sudo pacman -Syu --noconfirm
+for f in $(find pkg/ -name '*.txt' -or -name '*.txt'); do sudo pacman -S - < $f --noconfirm --needed; done
