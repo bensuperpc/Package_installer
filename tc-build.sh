b4c5416f2ea23d3c7f163f9f879c94242ac56f46
Bensuperpc <bensuperpc@gmail.com>
2021-06-08 12:09:30 +0200

    Add scripts
    Add scripts
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

---
+++ tc-build.sh
@@ -0,0 +1,13 @@
+sudo pacman -S base-devel \
+          bison \
+          ccache \
+          clang \
+          cmake \
+          flex \
+          git \
+          libelf \
+          lld \
+          ninja \
+          openssl \
+          python3 \
+          uboot-tools
