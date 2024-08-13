d5cd5e4abcd10496f68a20e82ec002aff5c9ab93
Bensuperpc <bensuperpc@gmail.com>
2024-08-13 18:36:01 +0200

    Update package
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- docker-postinstall.sh
+++ docker-postinstall.sh
@@ -2,4 +2,5 @@
 sudo groupadd docker || true
 sudo usermod -aG docker $USER || true
 sudo systemctl enable docker || true
+sudo systemctl start docker || true
 sudo docker run hello-world || true
--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@
 # ==============================================================================
 
 sudo pacman -Syu --noconfirm
-for f in $(find pkg/ -name '*.txt' -or -name '*.txt'); do sudo pacman -S - < $f --noconfirm --needed; done
+for f in $(find pacman_pkg/ -name '*.txt' -or -name '*.txt'); do sudo pacman -S - < $f --noconfirm --needed || true; done
