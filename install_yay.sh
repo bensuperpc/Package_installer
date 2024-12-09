6c422e5d08ff268a28f147d4d26e4af86a8ca68b
Bensuperpc <bensuperpc@gmail.com>
2024-12-09 23:04:40 +0100

    Add missing packages for yt-dlp
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- install_yay.sh
+++ install_yay.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
+#sudo pacman -S --needed git base-devel
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
+
+sudo pacman -Syu --noconfirm
+
+for f in $(find pacman_pkg/ -name '*.txt' -or -name '*.txt'); do yay -S - < $f --noconfirm --needed || true; done
